7c2e2af24a1285cc1b1099b7f7b1d818dddfc2d4
Adiraju Aditya Srivatsa <73454745+Adigateway@users.noreply.github.com>
2025-04-11 18:49:04 +0200

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-COMPANY_TICKER = "AAPL"  # Example (change to any ticker e.g., "MSFT", "TSLA")
+COMPANY_TICKER = "GOOG"  
 START_DATE = "2013-01-01"
-END_DATE = "2025-02-05"
+END_DATE = "2025-02-11"
 FRED_API_KEY = "0fe4fd721773979eeffadfe3819244ed" 
